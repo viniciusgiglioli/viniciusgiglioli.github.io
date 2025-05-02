0e1862a2399aa0c57c2c6d81275031d3e79b6804
viniciusgiglioli <viniciusgiglioli@gmail.com>
2025-05-01 23:27:13 -0300

    mod 9 mobile vX.1

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 function openTab(tabId) {
-    // Esconde todos os conteúdos das abas
     document.querySelectorAll('.tab-content').forEach(tab => {
         tab.classList.remove('active');
     });
-
-    // Remove a classe 'active' de todos os botões
     document.querySelectorAll('.tab-button').forEach(button => {
         button.classList.remove('active');
     });
-
-    // Mostra o conteúdo da aba selecionada
     document.getElementById(tabId).classList.add('active');
-
-    // Adiciona a classe 'active' ao botão clicado
     event.target.classList.add('active');
-
-    // Atualiza o subtítulo com o nome da aba
     const subtitle = document.getElementById('subtitle');
     subtitle.textContent = event.target.textContent;
+
+    // Fecha o menu automaticamente no mobile após selecionar um tema
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('active');
+    sidebar.classList.add('hidden');
 }
 
 function flipCard(card) {
@@ -44,13 +40,18 @@ function switchLanguage(lang) {
             back.textContent = card.getAttribute('data-back-en') || back.textContent;
         } else {
             front.innerHTML = card.getAttribute('data-original-front') + 
-                `<div class="icons"><i class="${icon1}" style="color: ${color};"></i><i class="${icon2}" style="color: ${color};"></i></div>`;
+                `<div class="icons"><i class="${icon1}" style="color: ${color};"></i><i class="${icon2}" style="color: #${color};"></i></div>`;
             back.textContent = card.getAttribute('data-original-back') || back.textContent;
         }
     });
 }
 
-// Abre a primeira aba por padrão e define o subtítulo
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+    sidebar.classList.toggle('hidden');
+}
+
 document.getElementById('prompt-engineering').classList.add('active');
 document.querySelector('.tab-button').classList.add('active');
 document.getElementById('subtitle').textContent = document.querySelector('.tab-button').textContent;
